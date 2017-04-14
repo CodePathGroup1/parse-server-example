e1e6c7c8f30d5af2c65ec9b041dd51b4fb234a5a
Thomas Zhu <dev@thomaszhu.com>
2017-04-13 17:51:09 -0700

    Added push notification cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,14 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define('push', function(request, response) {
+    var query = new Parse.Query(Parse.Installation);
+    query.equalTo("user", request.params.User);
+    
+    Parse.Push.send({
+        where: query,
+        data: {
+            alert: request.params.message,
+            badge: 'increment',
+            sound: ''
+        }
+    }, { useMasterKey: true });
 });
