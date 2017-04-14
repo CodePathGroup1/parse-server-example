ed44b1b0bd286496f0e896a76cb6542f0b2f5d40
Thomas Zhu <dev@thomaszhu.com>
2017-04-13 19:41:02 -0700

    Modified cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -6,6 +6,8 @@ Parse.Cloud.define('push', function(request, response) {
     var query = new Parse.Query(Parse.Installation);
     query.equalTo("user", user);
     
+    console.log(query);
+    
     Parse.Push.send({
         where: query,
         data: {
